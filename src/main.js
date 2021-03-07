d3750b2fe686c21b388a0631a03943af2d20dfe0
Javier Mendoza <javiermoron159@hotmail.com>
2021-03-06 21:11:24 -0500

    Instalación y configuración de Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+
+// Librería CSS
+import BootstrapVue from 'bootstrap-vue'
+
+// Archivos locales de la App
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// CSS Global
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
